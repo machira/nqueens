99c336e8b361891867c83e23f3bc7606aad2b6a9
machira <raymond.machira@gmail.com>
2014-11-13 08:06:08 -0500

    a few more tests added to results.

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         NUM_PIECES = int(sys.argv[1])
         if sys.argv > 2:
-            CONSTRAINT = True if (sys.argv[2]) else False
+            CONSTRAINT = True if int(sys.argv[2]) else False
     start_board = [0]* NUM_PIECES
 
     Nqueens.CONSTRAINT = CONSTRAINT
     Nqueens.BOARD_SIZE = NUM_PIECES
 
     final_board, is_solved = Nqueens.solve(start_board)
-
+    Nqueens.show_board(final_board)
     with open(OUTFILE,'a') as outfile:
         # outfile.write("Number of Pieces,Nodes Expanded,Solved,UsingConstraint\n")
         outfile.write("{0},{1},{2},{3}\n".format(NUM_PIECES,Nqueens.NUM_NODES_EXPANDED,is_solved,CONSTRAINT))
